ad5c674ad4e795b83c5aca7b3d8c8146b5b06460
librah <huang.libra@gmail.com>
2023-03-18 08:53:58 +0800

    get TW bond data from marketwatch

--- app_tw.py
+++ app_tw.py
@@ -19,17 +19,17 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def get_quicken_row(symbol, d, row):
+def get_quicken_row(row):
     """
     Reference: https://community.quicken.com/discussion/7542737/faq-importing-security-prices-including-hi-lo-vol/p1?new=1
     """
-    return '{symbol}tw, {close}, ---, {date}, ---, {hi}, {lo}, {vol}, *'.format(
-        symbol=symbol,
-        close=row[6],
-        date=d.strftime('%Y/%m/%d'),
-        hi=row[4],
-        lo=row[5],
-        vol=row[1] / 100
+    return "{symbol}tw, {close}, ---, {date}, ---, {hi}, {lo}, {vol}, *".format(
+        symbol=row.get('symbol'),
+        close=row.get('close'),
+        date=row.get('date').strftime('%Y/%m/%d'),
+        hi=row.get('hi'),
+        lo=row.get('lo'),
+        vol=row.get('vol') / 100
     )
 
 
@@ -48,6 +48,17 @@ def get_today():
         return dt, rows  # return data date, and dataframes
 
 
+def get_market_watch(symbol):
+    # ref: https://www.marketwatch.com/investing/fund/00773b/download-data?countrycode=tw
+    r = requests.get(f'https://www.marketwatch.com/investing/fund/{symbol.lower()}/downloaddatapartial?frequency=p1d&csvdownload=true&countrycode=tw')
+    if r.status_code != 200:
+        logger.error('Unable to get data from marketwatch: %s %s', r.status_code, r.text)
+        raise Exception('Unable to get data')
+    else:
+        rows = pd.read_csv(StringIO(r.text), index_col=0)
+        return rows
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Get ticker price")
     parser.add_argument('out', help='output file name')
@@ -63,9 +74,29 @@ def main(args):
             if not symbol or symbol.startswith('#'):
                 continue
             eprint('Getting "{symbol}" price'.format(symbol=symbol))
-            row = rows.loc[symbol]
-            f.write(get_quicken_row(symbol, d, row))
-            f.write('\n')
+            if symbol in rows.index:
+                row = rows.loc[symbol]
+                f.write(get_quicken_row({
+                    'symbol': symbol,
+                    'date': d,
+                    'vol': row[1],
+                    'hi': row[4],
+                    'lo': row[5],
+                    'close': row[6],
+                }))
+                f.write('\n')
+            else:
+                series = get_market_watch(symbol)
+                for series_date, series_data in series.iterrows():
+                    f.write(get_quicken_row({
+                        'symbol': symbol,
+                        'date': datetime.strptime(series_date, '%m/%d/%Y'),
+                        'vol': int(series_data[4].replace(',', '')),
+                        'hi': series_data[1],
+                        'lo': series_data[2],
+                        'close': series_data[3]
+                    }))
+                    f.write('\n')
 
 
 if __name__ == '__main__':
