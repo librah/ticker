2cc6f7080a4738d16edae9548f613036a4b23487
librah <huang.libra@gmail.com>
2024-03-12 21:10:05 +0800

    get tw otc data

--- app_tw.py
+++ app_tw.py
@@ -33,7 +33,7 @@ def get_quicken_row(row):
     )
 
 
-def get_today():
+def get_today_twse():
     # ref: https://data.gov.tw/dataset/11549
     r = requests.get('http://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_data')
     if r.status_code != 200:
@@ -45,6 +45,26 @@ def get_today():
         filename = params['filename']  # STOCK_DAY_ALL_20210312.csv
         dt = datetime.strptime(filename, 'STOCK_DAY_ALL_%Y%m%d.csv')
         rows = pd.read_csv(StringIO(r.text), index_col=0)
+        return dt, rows  # return data date, and dataframes
+
+
+def get_today_twotc():
+    # ref: https://data.gov.tw/dataset/11371
+    r = requests.get('https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430_result.php?l=zh-tw&se=EW&o=data')
+    if r.status_code != 200:
+        logger.error('Unable to get data from tpex: %s %s', r.status_code, r.text)
+        raise Exception('Unable to get data')
+    else:
+        # ex: Content-Disposition: "attachment;filename=SQUOTE_EW_1130312.csv"
+        value, params = cgi.parse_header(r.headers['Content-Disposition'])
+        filename = params['filename']  # SQUOTE_EW_1130312.csv
+        tw_date_str = filename.split('_')[2].split('.')[0]  # 1130312
+        if len(tw_date_str) != 7:
+            logger.error('Unable to determine otc data date: %s', filename)
+            raise Exception("Unable to determine otc data date")
+        year = int(tw_date_str[:3]) + 1911
+        dt = datetime.strptime(str(year) + tw_date_str[3:], '%Y%m%d')
+        rows = pd.read_csv(StringIO(r.text), index_col=1)
         return dt, rows  # return data date, and dataframes
 
 
@@ -67,22 +87,34 @@ def parse_args():
 
 
 def main(args):
-    d, rows = get_today()
+    twse_d, twse_rows = get_today_twse()
+    twotc_d, twotc_rows = get_today_twotc()
     with open(args.out, mode='w', encoding='utf-8') as f:
         for symbol in sorted(set(env.str('SYMBOLS_TW').split('\n'))):
             symbol = symbol.strip()
             if not symbol or symbol.startswith('#'):
                 continue
             eprint('Getting "{symbol}" price'.format(symbol=symbol))
-            if symbol in rows.index:
-                row = rows.loc[symbol]
+            if symbol in twse_rows.index:
+                row = twse_rows.loc[symbol]
                 f.write(get_quicken_row({
                     'symbol': symbol,
-                    'date': d,
+                    'date': twse_d,
                     'vol': row[1],
                     'hi': row[4],
                     'lo': row[5],
                     'close': row[6],
+                }))
+                f.write('\n')
+            elif symbol in twotc_rows.index:
+                row = twotc_rows.loc[symbol]
+                f.write(get_quicken_row({
+                    'symbol': symbol,
+                    'date': twotc_d,
+                    'vol': row[7],
+                    'hi': row[5],
+                    'lo': row[6],
+                    'close': row[2],
                 }))
                 f.write('\n')
             else:
